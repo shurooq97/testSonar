2a5dc93065356a9d62091f7e4ce9d6964a62e54b
hp <hp@shurooqGhaith>
2018-04-02 11:57:11 -0700

    dd

---
+++ src/test/SonarTest.java
@@ -0,0 +1,14 @@
+package test;
+
+public class SonarTest {
+
+	public static int fun(int x,int y){
+		if(x>y)return 1;
+		else return 0;
+		
+	}
+	
+	public static void main(String [] args){
+		System.out.print(fun(2,3));
+	}
+}
